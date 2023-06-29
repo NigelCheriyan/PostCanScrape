65e2faed00056a478b96366146b159b201e873a0
Nigel Cheriyan <cheriyan.nigel@protonmail.com>
2023-06-29 12:21:34 -0400

    Add existing file

---
+++ MainScraper.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 11:48:53 2023
+Web Scraper to double check all addresses with Canada Post
+
+For Dying with Dignity
+@author: nigel
+"""
+
+
+# Pull CSV file
+
+
+
+# Pull up Website
+
+
+
+# Check each row
+
+
+
+
+# Pull 
