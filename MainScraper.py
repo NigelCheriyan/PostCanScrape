7c19cb5ac2fc299522b5573aa4db0b89c35f4024
Nigel Cheriyan <cheriyan.nigel@protonmail.com>
2023-06-29 12:58:15 -0400

    changes to main scraper

--- MainScraper.py
+++ MainScraper.py
@@ -6,26 +6,77 @@ Web Scraper to double check all addresses with Canada Post
 For Dying with Dignity
 @author: nigel
 """
-
-
-# Pull CSV file
-file_name 
-
-
-# Pull up Website
-
-Url ="https://www.canadapost-postescanada.ca/cpc/en/tools/find-a-postal-code.page"
-
-
-#function to enter address into website and receive string
-
-
-#function to parse string data 
+import urllib, xml.dom.minidom
 
 
 
 
-# Check each row
+
+""" Pull CSV file"""
+
+file_name  = "Nigel Cheriyan Address Cleanup.xlsx"
+
+dfs = pd.read_excel(file_name, sheet_name=None) # pull data from file 
+
+
+""" Pull up Website ~~~ CODE FROM CAN POST API ADDRESS COMPLETE"""
+
+def AddressComplete_Interactive_Find_v2_10(Key, SearchTerm, LastId, SearchFor, Country, LanguagePreference, MaxSuggestions, MaxResults, Origin, Bias, Filters, GeoFence):
+
+      #Build the url
+      requestUrl = "http://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/Find/v2.10/xmla.ws?"
+      requestUrl += "&" +  urllib.urlencode({"Key":Key})
+      requestUrl += "&" +  urllib.urlencode({"SearchTerm":SearchTerm})
+      requestUrl += "&" +  urllib.urlencode({"LastId":LastId})
+      requestUrl += "&" +  urllib.urlencode({"SearchFor":SearchFor})
+      requestUrl += "&" +  urllib.urlencode({"Country":Country})
+      requestUrl += "&" +  urllib.urlencode({"LanguagePreference":LanguagePreference})
+      requestUrl += "&" +  urllib.urlencode({"MaxSuggestions":MaxSuggestions})
+      requestUrl += "&" +  urllib.urlencode({"MaxResults":MaxResults})
+      requestUrl += "&" +  urllib.urlencode({"Origin":Origin})
+      requestUrl += "&" +  urllib.urlencode({"Bias":Bias})
+      requestUrl += "&" +  urllib.urlencode({"Filters":Filters})
+      requestUrl += "&" +  urllib.urlencode({"GeoFence":GeoFence})
+
+      #Get the data
+      dataDoc = xml.dom.minidom.parseString(urllib.urlopen(requestUrl).read())
+
+      #Get references to the schema and data
+      schemaNodes = dataDoc.getElementsByTagName("Column")
+      dataNotes = dataDoc.getElementsByTagName("Row")
+
+      #Check for an error
+      if len(schemaNodes) == 4 and schemaNodes[0].attributes["Name"].value == "Error":
+         raise Exception, dataNotes[0].attributes["Description"].value
+
+      #Work though the items in the response
+      results = []
+      for dataNode in dataNotes:
+         rowData = dict()
+         for schemaNode in schemaNodes:
+              key = schemaNode.attributes["Name"].value
+              value = dataNode.attributes[key].value
+              rowData[key] = value
+         results.append(rowData)
+
+       return results
+
+      #FYI: The output is an array of key value pairs, the keys being:
+      #Id
+      #Text
+      #Highlight
+      #Cursor
+      #Description
+      #Next
+
+
+"""function to parse string data""" 
+
+
+
+
+
+"""Loop across all the clientel and check address"""
 
 
 
